d9337de1756f3180674e64035b370a880ae4f1fa
Sebastian Luetzow <sebastian.luetzow@gmail.com>
2016-06-10 09:24:19 +0200

    added backwards compatibility for main page link

---
+++ de/index.php
@@ -0,0 +1,7 @@
+<?php
+//Diese Datei dient nur der Kompatibilität zur alten Homepage, da der alte Link auf die Startseite alumpi.uni-bayreuth.de/de/index.php war.
+//Durch diese Weiterleitung funktionieren auch die alten Links noch.
+
+
+header('Location: ./startseite/index.php');
+?>
