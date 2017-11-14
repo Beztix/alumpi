2fe6c385bb5a83777551c95773f3574a05ac2958
Sebastian Luetzow <sebastian.luetzow@gmail.com>
2017-11-14 18:33:18 +0100

    updated site content of 2017 image gallery

--- bildergalerie_absolventenfeier2017/content.php
+++ bildergalerie_absolventenfeier2017/content.php
@@ -8,14 +8,14 @@ if(!defined('AccessConstant')) {die('Direct access not permitted');}
 		
 		
 			<section class="top_image">
-				<img src="../_images_content/banner_absolventen2016.jpg" alt="Absolventenfeier 2016">
+				<img src="../_images_content/banner_absolventen2017.jpg" alt="Absolventenfeier 2017">
 			</section>
 		
 		
 		
             <section class="text">
 			
-				<h1>Bildergalerie Absolventenfeier 2016</h1>
+				<h1>Bildergalerie Absolventenfeier 2017</h1>
 
 				<?php include_once('ubergallery-resources/UberGallery.php'); $gallery = UberGallery::init()->createGallery('ubergallery-images'); ?>
 				
