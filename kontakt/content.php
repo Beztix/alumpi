23118984b57d805e6227210a6a91af3068613a0b
Julian Neuberger <neuberger.julian@gmail.com>
2018-03-14 10:40:45 +0100

    added member portrait on contact page

--- kontakt/content.php
+++ kontakt/content.php
@@ -1,77 +1,148 @@
 <?php
 //Abfrage der in den index.php definierten Konstante, um direkten Zugriff auf diese PHP-Datei zu verhindern
-if(!defined('AccessConstant')) {die('Direct access not permitted');}
+if (!defined('AccessConstant'))
+{
+    die('Direct access not permitted');
+}
 ?>
 
-		<section id="content">
-		
-			<section class="top_image">
-				<img src="../_images_content/banner_vorstand_18.jpg" alt="Vorstand AluMPI">
-			</section>
-		
-            <section class="text">
+<section id="content">
 
-				<h1>Kontakt</h1>
+	<!-- Raus auf Armins Wunsch -->
+	<!--	<section class="top_image">-->
+	<!--		<img src="../_images_content/banner_vorstand_17.jpg" alt="Vorstand AluMPI">-->
+	<!--	</section>-->
 
-				<br>
-				<p>
-				Absolventen- und Förderverein MPI Uni Bayreuth e.V.<br>
-				Postfach aluMPI<br> 
-				Gebäude NWII<br> 
-				95440 Bayreuth<br>
-				<a href="mailto:alumpi@uni-bayreuth.de">alumpi@uni-bayreuth.de</a>
-				</p>
-				<br>
-				<br>
+	<section class="text">
 
-				<h2>Vorstand:</h2>
+		<h1>Kontakt</h1>
 
-				<table class="striped" style="width:100%;">
-					<colgroup>
-						<col style="width:30%;">
-						<col style="width:30%;">
-						<col style="width:40%;">
-					</colgroup>
-					<tr>
-						<td>1. Vorsitzender</td>
-						<td width="214">Armin Kögel M.Sc.</td>
-						<td width="348">Armin.Koegel (at) uni-bayreuth.de</td>
-					</tr>
-					<tr>
-						<td>2. Vorsitzender</td>
-						<td width="214">Markus Klar B.Sc.</td>
-						<td width="348">Markus.Klar (at) uni-bayreuth.de</td>
-					</tr>
-					<tr>
-						<td>Schatzmeister</td>
-						<td width="214">Dorian Rohner M.Sc.</td>
-						<td width="348">dorian.rohner (at) uni-bayreuth.de</td>
-					</tr>
-				</table>
-				<br>
-				<br>
+		<br>
+		<p>
+			Absolventen- und Förderverein MPI Uni Bayreuth e.V.<br>
+			Postfach aluMPI<br>
+			Gebäude NWII<br>
+			95440 Bayreuth<br>
+			<a href="mailto:alumpi@uni-bayreuth.de">alumpi@uni-bayreuth.de</a>
+		</p>
+		<br>
+		<br>
 
+		<h2>Vorstand:</h2>
 
-				<h2>Ansprechpartner:</h2>
+		<div class="portrait-list">
+			<div class="portrait-container">
+				<img src="../_images_content/Armin.JPG" class="portrait"/>
+				<div class="portrait-text">
+					<div class="portrait-name">Armin Kögel</div>
+					<div class="portrait-job">1. Vorsitzender</div>
+					<a class="portrait-mail" href="mailto:armin.koegel@alumpi.de">armin.koegel@alumpi.de</a>
+				</div>
+			</div>
 
-				<table class="striped" style="width:100%;">
-					<colgroup>
-						<col style="width:30%;">
-						<col style="width:30%;">
-						<col style="width:40%;">
-					</colgroup>
-					<tr>
-						<td>IT-Beauftragter</td>
-						<td>Sebastian Lützow B.Sc.</td>
-						<td>sebastian.luetzow (at) gmail.com</td>
-					</tr>
-				</table>
+			<div class="portrait-container">
+				<img src="../_images_content/Markus.JPG" class="portrait"/>
+				<div class="portrait-text">
+					<div class="portrait-name">Markus Klar</div>
+					<div class="portrait-job">2. Vorsitzender</div>
+					<a class="portrait-mail" href="mailto:markus.klar@alumpi.de">markus.klar@alumpi.de</a>
+				</div>
+			</div>
 
-				<br>
-				<br>
-				<br>
-			
-			</section>
-			
-        </section>
+			<div class="portrait-container">
+				<img src="../_images_content/deko_absolventenfeier.jpg" class="portrait placeholder"/>
+				<div class="portrait-text">
+					<div class="portrait-name">Dorian Rohner</div>
+					<div class="portrait-job">Schatzmeister</div>
+					<a class="portrait-mail" href="mailto:dorian.rohner@alumpi.de">dorian.rohner@alumpi.de</a>
+				</div>
+			</div>
+		</div>
+
+		<h2>Kuratorium:</h2>
+
+		<div class="portrait-list">
+			<div class="portrait-container">
+				<img src="../_images_content/Philip.JPG" class="portrait"/>
+				<div class="portrait-text">
+					<div class="portrait-name">Philip Krinninger</div>
+					<div class="portrait-job">Sprecher des Kuratoriums</div>
+				</div>
+			</div>
+
+			<div class="portrait-container">
+				<img src="../_images_content/deko_absolventenfeier.jpg" class="portrait placeholder"/>
+				<div class="portrait-text">
+					<div class="portrait-name">Sebastian Lützow</div>
+					<div class="portrait-job">Beauftragter IT</div>
+					<a class="portrait-mail" href="mailto:sebastian.luetzow@alumpi.de">sebastian.luetzow@alumpi.de</a>
+				</div>
+			</div>
+
+			<div class="portrait-container">
+				<img src="../_images_content/Maria.JPG" class="portrait"/>
+				<div class="portrait-text">
+					<div class="portrait-name">Maria Treimer</div>
+					<div class="portrait-job">Schriftführerin</div>
+				</div>
+			</div>
+
+			<div class="portrait-container">
+				<img src="../_images_content/Benno.JPG" class="portrait"/>
+				<div class="portrait-text">
+					<div class="portrait-name">Benno Hoch</div>
+					<div class="portrait-job">Beauftragter Social Media</div>
+				</div>
+			</div>
+
+			<div class="portrait-container">
+				<img src="../_images_content/Bianca.JPG" class="portrait"/>
+				<div class="portrait-text">
+					<div class="portrait-name">Bianca Bauer</div>
+				</div>
+			</div>
+
+			<div class="portrait-container">
+				<img src="../_images_content/Adal.JPG" class="portrait"/>
+				<div class="portrait-text">
+					<div class="portrait-name">Adal Sabri</div>
+				</div>
+			</div>
+
+			<div class="portrait-container">
+				<img src="../_images_content/Silvan.JPG" class="portrait"/>
+				<div class="portrait-text">
+					<div class="portrait-name">Silvan Schmutzler</div>
+				</div>
+			</div>
+
+			<div class="portrait-container">
+				<img src="../_images_content/deko_absolventenfeier.jpg" class="portrait placeholder"/>
+				<div class="portrait-text">
+					<div class="portrait-name">Ingo Schelter</div>
+				</div>
+			</div>
+
+			<div class="portrait-container">
+				<img src="../_images_content/Ulla.JPG" class="portrait"/>
+				<div class="portrait-text">
+					<div class="portrait-name">Ulla Kaltenhauser</div>
+				</div>
+			</div>
+
+			<div class="portrait-container">
+				<img src="../_images_content/Manu.JPG" class="portrait"/>
+				<div class="portrait-text">
+					<div class="portrait-name">Manuel Eisentraut</div>
+				</div>
+			</div>
+		</div>
+
+		<br>
+		<br>
+		<br>
+
+	</section>
+
+</section>
 		
