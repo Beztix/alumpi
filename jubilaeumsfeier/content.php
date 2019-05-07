75524ad4aab814461b8e5deab7608cd61c5eb555
Julian Neuberger <neuberger.julian@gmail.com>
2019-05-07 16:17:31 +0200

    updates jubilaeumsfeier page

--- jubilaeumsfeier/content.php
+++ jubilaeumsfeier/content.php
@@ -5,10 +5,12 @@ if(!defined('AccessConstant')) {die('Direct access not permitted');}
 
 <section id="content">
     <section class="top_image">
-        <img src="../_images_content/banner_sektglaeser.jpg" alt="Sektgläser">
+        <img src="../_images_content/savethedate_jubilaeumsball.jpg" alt="Sektgläser">
     </section>
 
     <section class="text">
-        Diese Seite befindet sich gerade im Aufbau. Schauen Sie doch bitte bald wieder vorbei!
+        Unser Verein feiert in diesem Jahr seinen 10. Geburtstag! Zu diesem Anlass laden wir alle Vereinsmitglieder, Angehörige und Ehemalige der Fakultät 1 und besonders alle aktuellen Absolventen zum Jubiläumsball am Samstag, den 2. November 2019, in das evangelischen Gemeindehaus Bayreuth ein. Nach dem Festakt mit Ehrung der diesjährigen Absolventen und dem Buffet sorgt eine Liveband für die richtige Stimmung. Nehmen Sie also Familie und Freunde mit, schwingen Sie das Tanzbein, treffen Sie Jung und Alt und gönnen Sie sich einen leckeren Cocktail der berühmten Physikerbar. Wir freuen uns auf Sie!<br>
+		<br>
+		Diese Seite wird ständig aktualisiert, schauen Sie also bald wieder vorbei!
     </section>
 </section>
